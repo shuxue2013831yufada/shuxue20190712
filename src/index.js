85b5235935a89925d9db6b29c49a251bd76c66a7
someone <someone@someplace.com>
2019-07-12 12:43:00 +0800

    two

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Button} from 'antd-mobile'
 
-ReactDOM.render(<Button type='primary'>按钮</Button>,document.getElementById('root'))
+ReactDOM.render(
+    <Button type='primary'>按钮</Button>,
+    document.getElementById('root'))
 
